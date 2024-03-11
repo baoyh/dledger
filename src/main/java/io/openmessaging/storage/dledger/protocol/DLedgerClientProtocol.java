665613465848adee25228ed4d68a0de2ac39cb25
baoyh <happyhbao@163.com>
2024-03-11 18:08:35 +0800

    init

---
+++ src/main/java/io/openmessaging/storage/dledger/protocol/DLedgerClientProtocol.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2017-2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.openmessaging.storage.dledger.protocol;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * Both the RaftLogServer(inbound) and RaftRpcService (outbound) should implement this protocol
+ */
+public interface DLedgerClientProtocol {
+
+    /**
+     * 客户端从服务器获取日志条目（获取数据）
+     */
+    CompletableFuture<GetEntriesResponse> get(GetEntriesRequest request) throws Exception;
+
+    /**
+     * 客户端向服务器追加日志（存储数据）
+     */
+    CompletableFuture<AppendEntryResponse> append(AppendEntryRequest request) throws Exception;
+
+    /**
+     * 获取元数据
+     */
+    CompletableFuture<MetadataResponse> metadata(MetadataRequest request) throws Exception;
+
+    /**
+     * leader关系转换
+     */
+    CompletableFuture<LeadershipTransferResponse> leadershipTransfer(LeadershipTransferRequest request) throws Exception;
+}
